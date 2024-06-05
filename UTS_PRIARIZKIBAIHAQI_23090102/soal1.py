be6a177be159d904e7abd8c0118b39acce8c6cec
Priarizkibaihaqi <145573100+Priarizkibaihaqi@users.noreply.github.com>
2024-06-05 19:17:21 +0700

    Add files via upload

---
+++ UTS_PRIARIZKIBAIHAQI_23090102/soal1.py
@@ -0,0 +1,11 @@
+jumlah_barang = int(input("Masukkan jumlah barang : "))
+
+total = 0
+while jumlah_barang > 0:
+    harga_barang = int(input(f"Masukkan harga barang : "))
+    if jumlah_barang == 0:
+        break
+    total += harga_barang
+    jumlah_barang -= 1
+
+print("Total Belanjaan :",f"Rp.{total:.0f}")
